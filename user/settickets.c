4c0a52309c8f014afd2bfbcfe915372c4ca863fa
Aaron Ripley <ripleya1@xavier.edu>
2023-10-17 11:48:22 -0400

    clean up settickets

--- user/settickets.c
+++ user/settickets.c
@@ -6,19 +6,9 @@ int
 main(int argc, char **argv)
 {
   int c;
-  int i;
-
-  if(argc < 2){
-    fprintf(2, "usage: settickets numtickets...\n");
+  c = settickets(atoi(argv[1]));
+  if(c == -1){
     exit(1);
-  }
-  for(i = 1; i < argc; i++){
-    c = settickets(atoi(argv[i]));
-    if(c == -1){
-      fprintf(2, "invalid\n");
-      exit(1);
-    }
-    printf("Tickets: %d\n", atoi(argv[i])); // print the number of tickets
   }
   exit(0);
 }
